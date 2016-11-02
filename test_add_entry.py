2961920495dcacb6e7bedc23f77e5a47bfc16352
assassinen <assassinen@ya.ru>
2016-11-02 16:37:20 +0300

    Задание №3: Выделение вспомогательных методов.
    Выполнил пункт:
    - Выделить вспомогательные методы, чтобы повысить читаемость тестов

--- test_add_entry.py
+++ test_add_entry.py
@@ -14,11 +14,11 @@ class test_add_entry(unittest.TestCase):
     def setUp(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
-    
-    def test_test_add_entry(self):
-        success = True
-        wd = self.wd
+
+    def open_home_page(self, wd):
         wd.get("http://localhost/addressbook/")
+
+    def login(self, wd):
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys("secret")
@@ -26,7 +26,11 @@ class test_add_entry(unittest.TestCase):
         wd.find_element_by_name("user").clear()
         wd.find_element_by_name("user").send_keys("admin")
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+
+    def add_book_entry(self, wd):
+        # open add address book entry
         wd.find_element_by_link_text("add new").click()
+        # fill address book entry form
         wd.find_element_by_name("firstname").click()
         wd.find_element_by_name("firstname").clear()
         wd.find_element_by_name("firstname").send_keys("Иван")
@@ -42,13 +46,22 @@ class test_add_entry(unittest.TestCase):
         wd.find_element_by_name("title").click()
         wd.find_element_by_name("title").send_keys("\\9")
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
+
+    def return_to_home_page(self, wd):
         wd.find_element_by_link_text("home page").click()
-        wd.find_element_by_link_text("Logout").click()
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").send_keys("\\undefined")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").send_keys("\\undefined")
+
+    def logout(self, success):
+        # logout
         self.assertTrue(success)
+
+    def test_test_add_entry(self):
+        success = True
+        wd = self.wd
+        self.open_home_page(wd)
+        self.login(wd)
+        self.add_book_entry(wd)
+        self.return_to_home_page(wd)
+        self.logout(success)
     
     def tearDown(self):
         self.wd.quit()
--- test_add_group.py
+++ test_add_group.py
@@ -26,7 +26,6 @@ class problem_test(unittest.TestCase):
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
-
         wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
 
     def open_groups_page(self, wd):
