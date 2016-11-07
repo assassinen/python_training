535b116c2b58d5a93f6874cffbd25e8ce7674ea5
assassinen <assassinen@ya.ru>
2016-11-07 17:37:18 +0300

    Фикстура
    Перевод на pytest

--- test_add_group.py
+++ test_add_group.py
@@ -1,28 +1,18 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
+import pytest
 from group import Group
 from application import Application
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
 
-class problem_test(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
+def test_problem_test(app):
+    app.login(username="admin", password="secret")
+    app.create_group(Group(name="test", header="test", footer="test"))
+    app.logout()
 
-    def test_problem_test(self):
-        self.app.login(username="admin", password="secret")
-        self.app.create_group(Group(name="test", header="test", footer="test"))
-        self.app.logout()
-
-    def tearDown(self):
-        self.app.destroy()
-
-if __name__ == '__main__':
-    unittest.main()
