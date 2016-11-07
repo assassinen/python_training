d7be2d00cc5891386aeb5a5655645830602aedee
assassinen <assassinen@ya.ru>
2016-11-07 18:01:18 +0300

    Фикстура (для book)
    избавиться от тестовых классов, сделать тесты обычными функциями, в
    которые фикстура передаётся в качестве параметра

--- test_add_entry.py
+++ test_add_entry.py
@@ -1,28 +1,16 @@
 # -*- coding: utf-8 -*-
-from selenium.webdriver.firefox.webdriver import WebDriver
-import unittest
+import pytest
 from book_entry import BookEntry
 from application import Application
 
-def is_alert_present(wd):
-    try:
-        wd.switch_to_alert().text
-        return True
-    except:
-        return False
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
 
-class test_add_entry(unittest.TestCase):
-    def setUp(self):
-        self.app = Application()
-
-    def test_test_add_entry(self):
-        self.app.login(username="admin", password="secret")
-        addEntry = BookEntry(firstname="Светлана", middlename="Иларионовна", lastname="Смирнова", work="безработный", address="Не дом и не улица", mobile='8915223344')
-        self.app.add_book_entry(addEntry)
-        self.app.logout()
-    
-    def tearDown(self):
-        self.app.destroy()
-
-if __name__ == '__main__':
-    unittest.main()
+def test_test_add_entry(app):
+    app.login(username="admin", password="secret")
+    addEntry = BookEntry(firstname="Алексей", middlename="Иларионовна", lastname="Баранцев", work="суперпрепод", address="Не дом и не улица", mobile='8915223344')
+    app.add_book_entry(addEntry)
+    app.logout()
