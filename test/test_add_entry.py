e45f76119b27b6bd1619eea8ed6368cd5526fd7a
assassinen <assassinen@ya.ru>
2016-11-09 17:11:34 +0300

    работа адресной кники с классом управления сессий

--- test/test_add_entry.py
+++ test/test_add_entry.py
@@ -3,14 +3,16 @@ import pytest
 from model.book_entry import BookEntry
 from fixture.application import Application
 
+
 @pytest.fixture
 def app(request):
     fixture = Application()
     request.addfinalizer(fixture.destroy)
     return fixture
 
+
 def test_test_add_entry(app):
-    app.login(username="admin", password="secret")
+    app.session.login(username="admin", password="secret")
     addEntry = BookEntry(firstname="Алексей", middlename="Иларионовна", lastname="Баранцев", work="суперпрепод", address="Не дом и не улица", mobile='8915223344')
     app.add_book_entry(addEntry)
-    app.logout()
+    app.session.logout()
