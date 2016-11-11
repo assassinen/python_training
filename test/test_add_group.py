f645862a7857afd459422ed462ee0cd911829eb2
assassinen <assassinen@ya.ru>
2016-11-11 16:38:20 +0300

    Делаем логин и логоут умными

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,5 +5,7 @@ from model.group import Group
 
 def test_add_group(app):
     app.group.create(Group(name="test", header="test", footer="test"))
+    app.session.logout()
+
     app.group.create(Group(name="test1", header="test1", footer="test1"))
 
