31faea1da5912b3347d9281b8b2804e5f18e4021
assassinen <assassinen@ya.ru>
2016-11-14 16:58:13 +0300

    Задание №9 (исправленное)
    Выполнено задание:
    Доработать тесты для удаления и модификации групп и контактов, чтобы они
    предварительно проверяли наличие хотя бы одного элемента, который можно
    удалить или модифицировать, и если такого элемента нет -- предварительно
    создавали его.

--- test/test_edit_group.py
+++ test/test_edit_group.py
@@ -4,5 +4,7 @@ __author__ = 'NovikovII'
 from model.group import Group
 
 def test_edit_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="test", header="test", footer="test"))
     app.group.edit_first_group(Group(name="editTest", header="editTest", footer="editTest"))
 
