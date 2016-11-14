dead273468a6e724aa3e9b0317214ec84e3900f7
assassinen <assassinen@ya.ru>
2016-11-14 15:01:56 +0300

    Задани 8
    фиксим ошибки

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -6,5 +6,5 @@ from model.group import Group
 def test_modify_group_name(app):
     app.group.modify(Group(name="modify_name"))
 
-#def test_modify_group_header(app):
-#    app.group.modify(Group(header="modify_header"))
+def test_modify_group_header(app):
+    app.group.modify(Group(header="modify_header"))
