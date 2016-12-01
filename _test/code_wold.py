ab8dd0a92df2e5f8458ec5f4f173114425efab6e
assassinen <assassinen@ya.ru>
2016-12-01 16:01:38 +0300

    Задание №14:  Исправление ошибок:
    Исправлены следующие замечания.

---
+++ _test/code_wold.py
@@ -0,0 +1,42 @@
+__author__ = 'NovikovII'
+
+import string
+import time
+import hashlib
+
+def testmd5():
+    st=time.time()
+    md5=hashlib.md5()
+    md5.update(bytes("string_var","UTF-8"))
+;
+    print(time.time()-st)
+
+testmd5()
+
+
+
+
+symbols = [i for i in string.ascii_letters[0:25]]
+
+#c_time = int(time.strftime("%Y%m%d%H%M%S", time.localtime()))*1000 + round(time.time() * 1000) % 1000
+c_time = int(time.strftime("%H%M%S", time.localtime()))
+
+print(c_time)
+
+
+a = ("".join((i1,i2,i3,i4,i5,i6,i7,i8,i9,i0))
+            for i1 in symbols
+            for i2 in symbols
+            for i3 in symbols
+            for i4 in symbols
+            for i5 in symbols
+            for i6 in symbols
+            for i7 in symbols
+            for i8 in symbols
+            for i9 in symbols
+            for i0 in symbols
+)
+n = 0
+
+md5 = 'ecbfb9d98a676f3d75f5931035a87207'
+print(len(md5))
--- test/test_phones.py
+++ test/test_phones.py
@@ -1,15 +1,21 @@
 __author__ = 'NovikovII'
 
 import re
+import random
 from model.contact import Contact
+
+def random_contact(app):
+    count = app.contact.count()
+    return random.randint(0, count-1)
 
 def test_phones_on_home_page(app):
     if app.contact.count() == 0:
         add_contact = Contact(firstname="Алексей", middlename="Иларионовна", lastname="Баранцев", work="суперпрепод", address="Не дом и не улица", mobile='8(915)223344')
         app.contact.add_contact(add_contact)
     #получаем список контактов с web-страницы
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_list_info_from_edit_page(0)
+    edit_contact = random_contact(app)
+    contact_from_home_page = app.contact.get_contact_list()[edit_contact]
+    contact_from_edit_page = app.contact.get_contact_list_info_from_edit_page(edit_contact)
     assert contact_from_home_page.parament['all_phone_from_home_page'] == merge_phone_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.parament['all_emal_from_home_page'] == merge_email_like_on_home_page(contact_from_edit_page)
     assert contact_from_home_page.parament['lastname'] == contact_from_edit_page.parament['lastname']
@@ -37,6 +43,5 @@ def merge_phone_like_on_home_page(contact):
 
 def merge_email_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear(x),
-                                filter(lambda x: x is not None,
-                                       [contact.parament['email'], contact.parament['email2'], contact.parament['email3']]))))
+                            filter(lambda x: x is not None,
+                                  [contact.parament['email'], contact.parament['email2'], contact.parament['email3']])))
