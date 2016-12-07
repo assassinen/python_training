1d718e3a4890ec3e8227dd41088f7c848ac37782
assassinen <assassinen@ya.ru>
2016-12-07 07:33:59 +0300

    Сделал генератор данных для групп
    Генератор созраняет данные в формате json

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,26 +3,9 @@ __author__ = 'NovikovII'
 # -*- coding: utf-8 -*-
 from model.group import Group
 import pytest
-import random
-import string
+from data.add_group import testdata
+from data.add_group import constant as testdata
 
-
-def random_string(prefix, maxlen):
-    symbols = string.ascii_letters + string.digits #+ string.punctuation + " "*10
-    return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
-
-#testdata = [Group(name="namea @RM&j ", header="header!4YJ,mAnXC,xfae B", footer="footerfQ 8C+THgLR#|")]
-#testdata = [Group(name="", header="header/IPWt5D'_<Q", footer="")]
-# testdata = [Group(name="", header="", footer="")] + [
-#     Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
-#     for i in range(5)
-# ]
-testdata = [
-    Group(name=name, header=header, footer=footer)
-    for name in ["", random_string("name", 10)]
-    for header in ["", random_string("header", 20)]
-    for footer in ["", random_string("footer", 20)]
-]
 
 @pytest.mark.parametrize("group", testdata, ids=[repr(x) for x in testdata])
 def test_add_empty_group(app, group):
