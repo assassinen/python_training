65af586189e00b60d349cadb719a54626b21b9ee
assassinen <assassinen@ya.ru>
2016-12-12 16:13:23 +0300

    Космктика после 20-го задания

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -18,15 +18,14 @@ def test_delete_some_contact(app, db, data_contacts, chech_ui):
         assert sorted(app.contact.get_contact_list(), key=Contact.id_or_max) == sorted(new_contact, key=Contact.id_or_max)
 
 
-# def test_delete_all_contact(app, db, chech_ui):
-#     #old_contact = app.contact.get_contact_list()
-#     old_contact = db.get_contact_list()
-#     while len(db.get_contact_list()):
-#         app.contact.delete_first_contact()
-#         assert len(old_contact) - 1 == app.contact.count()
-#         old_contact[0:1] = []
-#     #new_contact = app.contact.get_contact_list()
-#     new_contact = db.get_contact_list()
-#     if chech_ui:
-#         assert old_contact == new_contact
-#         assert 0 == len(new_contact)
+def test_delete_all_contact(app, db, chech_ui):
+    #old_contact = app.contact.get_contact_list()
+    old_contact = db.get_contact_list()
+    while len(db.get_contact_list()):
+        app.contact.delete_first_contact()
+        assert len(old_contact) - 1 == app.contact.count()
+        old_contact[0:1] = []
+    new_contact = db.get_contact_list()
+    if chech_ui:
+        assert old_contact == new_contact
+        assert 0 == len(new_contact)
