828143f1aa5e03bfa52d0f8b5fcafaa8895078fa
assassinen <ii484801>
2017-01-04 18:47:25 +0400

    Задание №21: Переделать тесты для проверки информации о контактах на главной странице Переделать тесты для проверки информации о контактах на главной странице -- на этот раз нужно реализовать сравнение для всех записей, а не для одной случайно выбранной. А сравнивать -- с информацией, загруженной из базы данных.

--- test/test_phones.py
+++ test/test_phones.py
@@ -22,6 +22,23 @@ def test_phones_on_home_page(app, data_contacts):
     assert contact_from_home_page.paramentr['address'] == contact_from_edit_page.paramentr['address']
 
 
+def test_phones_on_db(app, db, data_contacts):
+    if app.contact.count() == 0:
+        app.contact.add_contact(data_contacts)
+    #получаем список контактов с web-страницы
+    contact_from_home_page = app.contact.get_contact_list()
+    contact_from_db = db.get_contact_list()
+    contact_from_home_page.sort(key=Contact.id_or_max)
+    contact_from_db.sort(key=Contact.id_or_max)
+    assert len(contact_from_home_page) == len(contact_from_db)
+    for i in range(len(contact_from_home_page)):
+        assert contact_from_home_page[i].paramentr['all_phone_from_home_page'] == merge_phone_like_on_home_page(contact_from_db[i])
+        assert contact_from_home_page[i].paramentr['all_emal_from_home_page'] == merge_email_like_on_home_page(contact_from_db[i])
+        assert contact_from_home_page[i].paramentr['lastname'] == contact_from_db[i].paramentr['lastname']
+        assert contact_from_home_page[i].paramentr['firstname'] == contact_from_db[i].paramentr['firstname']
+        assert contact_from_home_page[i].paramentr['address'] == contact_from_db[i].paramentr['address']
+
+
 def test_phones_on_contact_view_page(app, data_contacts):
     if app.contact.count() == 0:
         app.contact.add_contact(data_contacts)
@@ -30,14 +47,17 @@ def test_phones_on_contact_view_page(app, data_contacts):
     contact_from_edit_page = app.contact.get_contact_list_info_from_edit_page(0)
     assert contact_from_view_page.paramentr['mobile'] == contact_from_edit_page.paramentr['mobile']
 
+
 def clear(s):
     return re.sub("[() -]", "", s)
+
 
 def merge_phone_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
                             map(lambda x: clear(x),
                                 filter(lambda x: x is not None,
                                        [contact.paramentr['home'], contact.paramentr['mobile'], contact.paramentr['work'], contact.paramentr['phone2']]))))
+
 
 def merge_email_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
