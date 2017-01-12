abc24e18bf3b19dd2595e7c0091d51493c36b1e3
assassinen <assassinen@ya.ru>
2017-01-12 15:38:11 +0300

    orm

---
+++ check_orm_fixture_connection.py
@@ -0,0 +1,16 @@
+__author__ = 'NovikovII'
+
+__author__ = 'NovikovII'
+
+from fixture.orm import ORMFixture
+
+db = ORMFixture(host="127.0.0.1", database="addressbook", user="root", password="")
+
+try:
+    l = db.get_contact_list()
+    for item in l:
+        print(item)
+    print(len(l))
+finally:
+    pass
+
